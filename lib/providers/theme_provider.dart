3d7092669aba6af9ed7093d38dbdc1ea4c22d123
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-05-31 14:19:24 -0300

    feat: criação de themeProvider para controlar a troca de tema claro/escuro

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,2 +1,21 @@
 import 'package:flutter/material.dart';
 
+class ThemeProvider extends ChangeNotifier{
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  void setThemeMOde(ThemeMode mode){
+    _themeMode = mode;
+    notifyListeners();
+  }
+
+  void toggleTheme(){
+    if(_themeMode == ThemeMode.dark){
+      _themeMode = ThemeMode.light;
+    } else{
+      _themeMode = ThemeMode.dark;
+    }
+    notifyListeners();
+  }
+}
