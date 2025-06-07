ea184a46961d58b16f689e29fbd881df11ea159c
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-06-07 13:21:33 -0300

    feat: adiciona parâmetros id e nome ao cosntrutor de MovimentacoesScreen

--- lib/screens/movimentacoes_screen.dart
+++ lib/screens/movimentacoes_screen.dart
@@ -3,5 +3,12 @@ import 'package:provider/provider.dart';
 import 'package:tp2/database/app_database.dart';
 
 class MovimentacoesScreen extends StatefulWidget{
-  
+  final int produtoId;
+  final String produtoNome;
+
+  const MovimentacoesScreen({
+    super.key,
+    required this.produtoId,
+    required this.produtoNome,
+  });
 }
